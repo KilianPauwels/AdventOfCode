40d0c122a0e696eeab6d516fe5d441e4c018ef52
kilianpauwels <kilianpauwels@gmail.Com>
2021-12-04 15:26:17 +0100

    day2.2

--- Day2/Day2.2/Day2.2/Program.cs
+++ Day2/Day2.2/Day2.2/Program.cs
@@ -1,9 +1,10 @@
-﻿string directory = @"C:\Users\kilia\source\repos\AdventOfCode\Day2\Day2.2\Day2.2\Input.txt";
+﻿string directory = @"C:\Users\kilia\source\repos\Day2.1\Day2.1\Input.txt";
 List<string> stringList = new List<string>();
 StreamReader sr = new StreamReader(directory);
 int hPos = 0, depth = 0;
 string[] input;
 int result = 0;
+int aim = 0;
 
 
 try
@@ -29,14 +30,15 @@ foreach(string s in stringList)
     if(input[0] == "forward")
     {
         hPos = hPos + Int32.Parse(input[1]);
+        depth = depth + aim * Int32.Parse(input[1]);
     }
     else if(input[0] == "down")
     {
-        depth = depth + Int32.Parse(input[1]);
+        aim = aim + Int32.Parse(input[1]);
     }
     else if(input[0] == "up")
     {
-        depth = depth - Int32.Parse(input[1]);
+        aim = aim - Int32.Parse(input[1]);
     }
     else
     {
